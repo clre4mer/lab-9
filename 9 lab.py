f7f1ccef51687d9e101c1fdffe3c9520c6394458
clre4mer <113285084+clre4mer@users.noreply.github.com>
2022-11-23 16:45:54 +0500

    обновил прогу

--- 9 lab.py
+++ 9 lab.py
@@ -17,21 +17,23 @@ N = - 1
 while N == -1:
     N = int(input('Начало диапазона возможных значений = 1, введите конец диапазона возможных значений:\n'))
     N = errorCheck(N)
-logging.info('Ввод значения прошёл успешно')
+logging.info(f'Пользователь ввёл значение {N} как конец диапазона возможных значений')
 
 k = -1
 while k == -1:
     k = int(input('Введите количество попыток:\n'))
     k = errorCheck(k)
-logging.info('Ввод значения прошёл успешно')
+logging.info(f'Пользователь ввёд число {k} как количество попыток')
 
 chislo = random.randrange(1,N+1,1)
+logging.info(f'Загадано число {chislo}')
+turn =1
 for i in range(k):
     num = -1
     while num == -1:
         num = int(input(f'Попробуйте угадать число от 1 до {N}:\n'))
         num = errorCheck(num)
-    logging.info('Ввод значения прошёл успешно')
+    logging.info(f'Пользователь ввёл число {num} как вариант отгадки')
     if num > chislo:
         print('Ваше число больше загаданного\n')
         logging.info('Пользователь не отгадал число')
@@ -42,3 +44,5 @@ for i in range(k):
         print('Невероятно, вы угадали, вашей интуиции позавидовал бы каждый!\n')
         logging.info('Пользователь отгадал число')
         break
+    logging.info(f'Пользователь использовал {turn} из {k} попыток')
+    turn +=1
